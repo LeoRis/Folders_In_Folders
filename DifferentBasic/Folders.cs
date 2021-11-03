dbedfbaf9f51d8efae9eae3c488579fee8b9f4f8
Leo Ris <ristanovski@hotmail.com>
2021-11-03 11:08:33 +0100

    Adjustments on creation of subfiles.

--- DifferentBasic/Folders.cs
+++ DifferentBasic/Folders.cs
@@ -23,7 +23,7 @@ namespace DifferentBasic
                     for (int j = 1; j <= 10; j++)
                     {
                         DirectoryInfo subDirectory= Directory.CreateDirectory(folder + counter + "\\" + j);
-                        for(int k = 1; k <= 10; k++)
+                        for(int k = 0; k < 10; k++)
                         {
                             FileStream subFile = File.Create(folder + counter + "\\" + j + "\\" + k + extensions[random.Next(extensions.Length)]);
                             subFile.Close();
