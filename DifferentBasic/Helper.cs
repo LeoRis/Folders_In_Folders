a5ade7e81531e62b212eb6b36274f7bee6a9ad26
Leo Ris <ristanovski@hotmail.com>
2021-11-04 19:23:49 +0100

    Adding new methods.

---
+++ DifferentBasic/Helper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace DifferentBasic
+{
+    public class Helper
+    {
+        #region FilesAndFolders
+        public static void RemoveFilesWithExtension(string folderPath, string fileExtension)
+        {
+            String folder = Path.GetDirectoryName(folderPath);
+
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Directory exists.");
+            }
+            else
+            {
+                Console.WriteLine("Directory doesn't exist.");
+            }
+
+
+
+            //var files = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories);
+
+            //for(int i = 0; i < files.Length; i++)
+            //{
+            //    if (files[i].Contains(fileExtension))
+            //    {
+            //        File.Delete(files[i]);
+            //    }
+            //}
+
+            //Console.WriteLine("Files removed.");
+        }
+
+        public static bool CheckIfPathIsFile(string filePath)
+        {
+            FileAttributes attr = File.GetAttributes(filePath);
+
+            //detect whether its a directory or file
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                Console.WriteLine("It's a directory");
+            }
+            else
+            {
+                Console.WriteLine("It's a file");
+                return true;
+            }
+            return false;
+        }
+
+        //public static void BackupMethod()
+
+        #endregion
+
+        #region Numbers
+
+
+
+        #endregion
+    }
+}
--- DifferentBasic/Program.cs
+++ DifferentBasic/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DifferentBasic
 {
@@ -11,10 +13,36 @@ namespace DifferentBasic
         {
             var path = @"C:\Homework\basic\";
             var longerPath = @"C:\Homework\";
+            var filePath = @"C:\Homework\basic1\1\1.pdf";
+
+            // TODO while()
+
+            if (Helper.CheckIfPathIsFile(longerPath))
+            {
+                if (Directory.Exists(longerPath))
+                {
+                    Console.WriteLine("Directory exists.");
+                }
+                else
+                {
+                    Console.WriteLine("Directory doesn't exist.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter folder path.");
+            }
+
+
 
             //Folders.CreateFolders(path);
 
-            UserInput.RemoveFilesWithExtension(longerPath);
+
+            //Console.WriteLine("Please insert the type of extension that you wish to delete: ");
+
+            //var extension = Console.ReadLine();
+
+            //Helper.RemoveFilesWithExtension(filePath, extension);
 
             //EvenDocumentChangeName.Changer(longerPath);
 
