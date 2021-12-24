afcb9a0d08c55ac25157c1ea93d52275d5fa2d12
Leo Ris <ristanovski@hotmail.com>
2021-12-24 21:46:37 +0100

    Adding Unit Tests

---
+++ DifferentBasic.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DifferentBasic.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DifferentBasic.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b88a3312-379a-4313-b9e5-42c5828ae458")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DifferentBasic.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DifferentBasic.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        // Convention to use for test methods names: MethodUnderTest_Scenario_ExpectedBehavior
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
